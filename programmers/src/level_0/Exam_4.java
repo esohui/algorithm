a11a1160c86c693ecbe18064cf331991b346914a
이소희 <lsh01020@gmail.com>
2022-10-06 23:03:24 +0900

    [프로그래머스] 코딩테스트 입문 Day 3 진행중

---
+++ programmers/src/level_0/Exam_4.java
@@ -0,0 +1,25 @@
+package level_0;
+
+/*
+ * Day 3 사칙연산, 배열, 수학
+ * - 나머지 구하기
+ */
+
+public class Exam_4 {
+	
+	public static void main(String[] args) {
+		Exam_4 exam = new Exam_4();
+		
+		// (1) 나머지 구하기
+		exam.remainder(3, 2);
+		
+	
+	}
+	
+	public int remainder(int num1, int num2) {
+        return num1 % num2;
+    }
+}
+
+
+
